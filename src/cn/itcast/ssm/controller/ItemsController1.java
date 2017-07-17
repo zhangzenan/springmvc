2a3e6ba1ae9b9cec8d46a0b6f5e2c86e695be0a0
zhangzenan <zzn1991129@126.com>
2017-07-17 22:06:04 +0800

    添加controller

--- src/cn/itcast/ssm/controller/ItemsController1.java
+++ src/cn/itcast/ssm/controller/ItemsController1.java
@@ -1,5 +1,17 @@
 package cn.itcast.ssm.controller;
 
-public class ItemsController1 {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ItemsController1 implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
